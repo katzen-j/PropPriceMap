5736f0e0bea26116a5a96488e5165b19f468e0ac
Joshua Katzenberg <josh.katzenberg@gmail.com>
2022-06-21 09:56:32 +0100

    working streamlit and refactored code

--- PropPriceMap/get_from_sql.py
+++ PropPriceMap/get_from_sql.py
@@ -12,7 +12,7 @@ def get_user_input(user_input):
     """
     user_input = user_input.upper()
     # create connection to SQL database
-    conn = sqlite3.connect('../SQL_data/gov_housing_data_lite')
+    conn = sqlite3.connect('SQL_data/gov_housing_data_lite')
     c = conn.cursor()  
 
     # first check for town/city:
@@ -64,71 +64,3 @@ def get_user_input(user_input):
         else:
             return "none"
         
-def town_city_connect(towncity):
-    """
-    Input a town or city and return a dataframe of
-    house prices in that area
-    """
-    # create connection to SQL database
-    conn = sqlite3.connect('raw_data/gov_housing_data_SQL')
-    c = conn.cursor()
-    
-    query = """
-    SELECT *
-    FROM gov_data_db as gd
-    WHERE gd.town_city = ?
-    """
-    c.execute(query, (towncity,))
-    datarows = c.fetchall()
-    datarows = pd.DataFrame(datarows, columns=df_cols)
-    
-    c.close()
-    conn.close()
-    
-    return datarows
-
-def locality_connect(locality):
-    """
-    Input a locality and return a dataframe of
-    house prices in that area
-    """
-    # create connection to SQL database
-    conn = sqlite3.connect('raw_data/gov_housing_data_SQL')
-    c = conn.cursor()
-    
-    query = """
-    SELECT *
-    FROM gov_data_db as gd
-    WHERE gd.locality = ?
-    """
-    c.execute(query, (locality,))
-    datarows = c.fetchall()
-    datarows = pd.DataFrame(datarows, columns=df_cols)
-    
-    c.close()
-    conn.close()
-    
-    return datarows
-
-def postcode_connect(postcode_user):
-    """
-    Input a postcode, partial or full and return a dataframe of
-    house prices in that postcode
-    """
-    # create connection to SQL database
-    conn = sqlite3.connect('raw_data/gov_housing_data_SQL')
-    c = conn.cursor()
-    
-    query = """
-    SELECT *
-    FROM gov_data_db as gd
-    WHERE gd.postcode LIKE ?
-    """
-    c.execute(query, (postcode_user+"%",))
-    datarows = c.fetchall()
-    datarows = pd.DataFrame(datarows, columns=df_cols)
-    
-    c.close()
-    conn.close()
-    
-    return datarows
